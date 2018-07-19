10eb453fcd0fbf9e306512feb1a6b857589c0d83
yinyongzhen <eagle_01yz@163.com>
2018-07-19 09:20:09 +0800

    网络请求，获取标题，并展示

---
+++ YZNews/YZNews/Home/HotPageViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HotPageViewController.swift
+//  YZNews
+//
+//  Created by yongzhen on 2018/7/18.
+//  Copyright © 2018年 yongzhen. All rights reserved.
+//
+
+import Foundation
---
+++ YZNews/YZNews/Model/models.swift
@@ -0,0 +1,9 @@
+//
+//  models.swift
+//  YZNews
+//
+//  Created by yongzhen on 2018/7/18.
+//  Copyright © 2018年 yongzhen. All rights reserved.
+//
+
+import Foundation
